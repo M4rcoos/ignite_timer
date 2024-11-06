d082c79d981413ceed0062a86556e37b5be53167
M4rcoosV <marcosvinicius@primi.com.br>
2024-11-06 19:56:24 -0300

    fix:formatando data

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,8 @@
-import { HeaderContainer } from "./styles";
-import { Timer, Scroll } from "phosphor-react";
-import logoIgnite from "../../assets/logo-ignite.svg";
-import { NavLink } from "react-router-dom";
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { HeaderContainer } from './styles'
 
 export function Header() {
   return (
@@ -16,5 +17,5 @@ export function Header() {
         </NavLink>
       </nav>
     </HeaderContainer>
-  );
+  )
 }
